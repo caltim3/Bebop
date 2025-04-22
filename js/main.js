6860c05575cbbfcfdedad24583d1967dff6313ce
Cannonball <75701795+caltim3@users.noreply.github.com>
2025-04-21 20:02:38 -0400

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,13 +1,14 @@
 // js/main.js
 import { AppState } from './app-state.js';
-import { UI } from './core/ui-manager.js';
-import { AudioContextManager } from './core/audio-context.js';
+import { UI } from '../core/ui-manager.js';
+import { AudioContextManager } from '../core/audio-context.js';
 import { createFretboard, updateFretboardNotes } from './fretboard.js';
 import { createBeats, onMetronomeInstrumentChange } from './metronome.js';
 import { loadProgression, updateProgressionKey, addMeasure, removeMeasure } from './chord-progression.js';
 import { initializeFretFlow } from './fretflow.js';
-import { log, ensureAudioInitialized } from './utils/helpers.js';
-import { TUNINGS } from './utils/constants.js';
+import { log, ensureAudioInitialized, suggestScaleForQuality, updateLoadingStatus } from '../utils/helpers.js';
+import { TUNINGS } from '../utils/constants.js';
+import { startPlayback, stopPlayback } from './playback.js';
 
 // Initialization
 async function initializeApp() {
