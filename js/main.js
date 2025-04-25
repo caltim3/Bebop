c6efa3330f994f6dbd846e87c738a6b135402043
Cannonball <75701795+caltim3@users.noreply.github.com>
2025-04-25 19:16:38 -0400

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,13 +1,13 @@
-import { AppState } from './core/app-state.js';
-import { UI } from './core/ui-manager.js';
-import { AudioContextManager } from './core/audio-context.js';
-import { createFretboard, updateFretboardNotes } from './core/fretboard.js';
-import { createBeats, onMetronomeInstrumentChange } from './core/metronome.js';
-import { loadProgression, updateProgressionKey, addMeasure, removeMeasure } from './core/chord-progression.js';
-import { initializeFretFlow } from './core/fretflow.js';
-import { log, ensureAudioInitialized, suggestScaleForQuality, updateLoadingStatus } from './utils/helpers.js';
-import { TUNINGS } from './utils/constants.js';
-import { startPlayback, stopPlayback } from './core/playback.js';
+import { AppState } from './app-state.js';
+import { UI } from './ui-manager.js';
+import { AudioContextManager } from './audio-context.js';
+import { createFretboard, updateFretboardNotes } from './fretboard.js';
+import { createBeats, onMetronomeInstrumentChange } from './metronome.js';
+import { loadProgression, updateProgressionKey, addMeasure, removeMeasure } from './chord-progression.js';
+import { initializeFretFlow } from './fretflow.js';
+import { log, ensureAudioInitialized, suggestScaleForQuality, updateLoadingStatus } from './helpers.js';
+import { TUNINGS } from './constants.js';
+import { startPlayback, stopPlayback } from './playback.js';
 
 document.addEventListener('DOMContentLoaded', async () => {
     await initializeApp();
